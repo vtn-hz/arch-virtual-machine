99db65ac84791d5267302620584c95e671662122
vtn-hz <juaniyvalen1234@gmail.com>
2025-09-03 22:56:56 -0300

    refactor: encapsulate error handlers

--- include/error_handler.h
+++ include/error_handler.h
@@ -1,3 +1,6 @@
+/**
+* migrate implementation to .c later (preguntar)
+*/
 
 #ifndef __ERROR_HANDLER__ 
 #define __ERROR_HANDLER__
@@ -27,5 +30,25 @@ void divisionByZero() {
     exit(EXIT_FAILURE);
 }
 
+
+/** 
+*   todo: add more parameters to send
+*   exception data   
+*/
+typedef void(*p_error_handler)();
+
+struct {
+    p_error_handler fileNotFound; 
+    p_error_handler invalidHeader;
+    p_error_handler invalidInstruction;
+    p_error_handler segmentationFault;
+    p_error_handler divisionByZero;
+} error_handler = {
+    fileNotFound, 
+    invalidHeader, 
+    invalidInstruction,
+    segmentationFault, 
+    divisionByZero
+};
 
 #endif 
