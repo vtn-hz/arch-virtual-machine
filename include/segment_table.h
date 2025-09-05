0fb89d2841105bd9830517971f819841ef3de06d
vtn-hz <juaniyvalen1234@gmail.com>
2025-09-05 00:11:07 -0300

    feat: elemental  interface tableSegment entity

--- include/segment_table.h
+++ include/segment_table.h
@@ -6,4 +6,22 @@ typedef struct {
     unsigned short size;
 } ST;
 
+
+/**
+*   Devuelve recibe la memoria logica con su correspondiente offset
+*   aplica la base correspondiente enviada por el tableSegment
+*   pasandola de logica a fisica
+*   @return int
+*/
+int transformAddress (ST tableSegment, int offset);
+
+/**
+*   Devuelve 1 o 0 (bool) acorde a si tiene el accesso permitido 
+*   a dicha memoria o no (Segmentation Fault)
+*   @return bool
+*/ 
+int isAccessAllowed (ST tableSegment, int offset) ;
+
+
+
 #endif
