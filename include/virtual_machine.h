554d2c4d5e3f298836ef4e78e4d05d34f1293a85
vtn-hz <juaniyvalen1234@gmail.com>
2025-09-06 14:28:37 -0300

    feat: added descriptive posible future change

--- include/virtual_machine.h
+++ include/virtual_machine.h
@@ -6,8 +6,20 @@
 typedef struct {
     char* mem;
     int reg[32];
-    TS table_seg[8];
-    // mnemonics
+    // POSIBLE CAMBIO
+    TS table_seg[8];  // TSS *table_segs
+    /**
+     * Se podria llegar a tratar internamente
+     * como una pila, donde al agregar un nuevo
+     * segmento suma el valor del anterior
+     * teniendo un "tope" 
+     * 
+     * TSS->add( size ) : return TS
+     * TSS->getTs( indice ) : TS
+     */
+
+
+    p_instruction instructions[32];
 } VirtualMachine;
 
 #endif
