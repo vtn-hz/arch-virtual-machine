279acd7e134ddf0a7fcc0330f34ac117f7c4bfa2
sim-schleider <simschleider@gmail.com>
2025-09-07 21:37:25 -0300

    feat: read all instructions in file

--- src/instructions.c
+++ src/instructions.c
@@ -2,50 +2,58 @@
 #include <stdlib.h>
 #include <instructions.h>
 
-void getInstruction(FILE *, char *, char *, char *, int *);
-void getOperands(FILE *, char*, char*, char, char, int);
+void getInstruction(FILE *, char *, char *, char *, int *, int *);
+void getOperands(FILE *, int *, int *, char, char, int);
 
 void instructor() {
-    FILE * vmx25 = fopen("sample.vmx", "rb");
+    FILE * vmx25 = fopen("test/sample.vmx", "rb");
     if (!vmx25) {
         printf("No se pudo abrir el archivo\n");
         // call error handler
     } else {
-        // advance pointer to instruction
-        fseek(vmx25, 8, 0); // hardcoded (8, 11, 17)
+        fseek(vmx25, 8, 0); // first instruction
 
-        char cod, tp1, tp2, op1, op2;
-        int n;
+        char cod, tp1, tp2;
+        int op1, op2, n;
 
-        getInstruction(vmx25, &cod, &tp1, &tp2, &n);
-        getOperands(vmx25, &op1, &op2, tp1, tp2, n);
-        // process information and loop
+        int flag = 1;
+        getInstruction(vmx25, &cod, &tp1, &tp2, &n, &flag);
+        while (flag) {
+            op1 = op2 = 0;
+            getOperands(vmx25, &op1, &op2, tp1, tp2, n);
+            // process information
+            // printf("mnm: %02X, op1: %08X, op2: %08X, num: %d\n", cod, op1, op2, n);
 
-        //printf("mnm: %02X\ntp1: %02X\ntp2: %02X\nop#: %d\n", cod, tp1, tp2, n);
+            getInstruction(vmx25, &cod, &tp1, &tp2, &n, &flag);
+        }
     }
 
     fclose(vmx25);
 }
 
-void getInstruction(FILE * vmx, char * cod, char * tp1, char * tp2, int * n) {
+void getInstruction(FILE * vmx, char * cod, char * tp1, char * tp2, int * n, int * flag) {
     char c;
-    fread(&c, 1, 1, vmx);
-    *cod = c & 0x1F; // 00011111
-
-    c >>= 4;
-    if (c & 0x01) {
-        *tp1 = c & 0x03;
-        *tp2 = c >> 2 & 0x03;
-        *n = 2;
+    if (!fread(&c, 1, 1, vmx)) {
+        *flag = 0;
     } else {
-        *tp1 = c >> 2 & 0x03;
-        *tp2 = 0;
-        *n = *tp1 > 0;
+        *cod = c & 0x1F; // 00011111
+
+        c >>= 4;
+        if (c & 0x01) {
+            *tp1 = c & 0x03;
+            *tp2 = c >> 2 & 0x03;
+            *n = 2;
+        } else {
+            *tp1 = c >> 2 & 0x03;
+            *tp2 = 0;
+            *n = *tp1 > 0;
+        }
     }
 }
 
-void getOperands(FILE * vmx, char * op1, char * op2, char tp1, char tp2, int n) {
-    int oparr[2], optarr[2] = {tp1, tp2};
+void getOperands(FILE * vmx, int * op1, int * op2, char tp1, char tp2, int n) {
+    int * oparr[2] = {op1, op2};
+    char optarr[2] = {tp1, tp2};
 
     for (int i = n; i > 0; i--) {
         char buf[3] = {0};
@@ -57,9 +65,7 @@ void getOperands(FILE * vmx, char * op1, char * op2, char tp1, char tp2, int n) 
             opaux |= buf[j];
         }
 
-        //printf("[ %06X ]\n", opaux);
-        oparr[i-1] = opaux;
+        *oparr[i-1] = opaux;
+        // if the value is immediate and negative, the int saves a byte too many (it should be, at most, three)
     }
-
-    printf("op1: %06X\nop2: %06X\n", oparr[0], oparr[1]);
 }
