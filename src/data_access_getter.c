e7dcc6bb5654cc3ade06f1d64cf985c607c24202
vtn-hz <juaniyvalen1234@gmail.com>
2025-09-11 00:18:00 -0300

    feat: data setter implemented

--- src/data_access_getter.c
+++ src/data_access_getter.c
@@ -35,16 +35,26 @@ int getDataFromMemory (VirtualMachine *virtualM, int operand, int bytes) {
     int logicMemoryAccess = virtualM->reg[ baseRegister ] + memoryOffset;
     int fisicMemoryAccess = 20 + memoryOffset; /*transformAddress(virtualM->table_seg, logicMemoryAccess );*/ // doesnt existst yet
 
+    // call memoryUpdateHandler( EVENT::GET, ...REQUIRED PARAMS)
+
     int readedData = 0 ;
     for  (int i=0 ; i<bytes ; i++) {
+        /**
+         * CUIDADO!!! AL NO UTILIZAR transformAddress, TE ESTAS SALTANDO EL CONTROL DE SEGMENTOS 
+         * */ 
         readedData = (readedData << 8) | virtualM->mem[ fisicMemoryAccess +  i ];        
     }
 
     return spreadSign( readedData, 32-bytes*8 );
 }
 
+int getDataFromEmpty (VirtualMachine *virtualM, int operand, int bytes) {
+    error_handler.invalidInstruction();
+    return 0;
+}
+
 void initializeGetters () {
-    availableDataGetter[0] = NULL;
+    availableDataGetter[0] = getDataFromEmpty;
     availableDataGetter[1] = getDataFromRegister;
     availableDataGetter[2] = getDataFromInmediato;
     availableDataGetter[3] = getDataFromMemory;
--- src/data_access_setter.c
+++ src/data_access_setter.c
@@ -10,14 +10,64 @@
 
 static p_setter_data availableDataSetter[4];
 
-void setData(VirtualMachine*, int operand, int value, int bytes) {
+void setData(VirtualMachine* virtualM, int operand, int value, int bytes) {
+    int operandType = extractOperationType( operand );
 
+    if (!(0 <= operandType && operandType <= 3)) {
+        error_handler.invalidInstruction();   
+    }
+    
+    availableDataSetter[ operandType ](virtualM, operand, value, bytes);
 }   
 
-void setDataToMemory(VirtualMachine*, int operand, int value, int bytes) {
+
+void setDataToRegister(VirtualMachine *virtualM, int operand, int value, int bytes) {
+    virtualM->reg[ extractOperationValue(operand) ] = value;
+}
+
+void setDataToInmediato (VirtualMachine *virtualM, int operand, int value, int bytes) {
+    error_handler.invalidInstruction();
+}
+
+void setDataToMemory(VirtualMachine* virtualM, int operand, int value, int bytes) {
+    int baseRegister = extractOperationBaseRegister( operand );
+    int memoryOffset = extractOperationValue( operand );
+
+    int logicMemoryAccess = virtualM->reg[ baseRegister ] + memoryOffset;
+    int fisicMemoryAccess = 20 + memoryOffset; /*transformAddress(virtualM->table_seg, logicMemoryAccess );*/ // doesnt existst yet
+
+    /**
+     * suponiendo que el usuario quiere guardar 2 bytes 
+     * deberiamos tomar el valor desde el medio y guardar los 2 ultimos
+     * o comenzar desde el principio y guardar los 2 primeros
+     * 
+     *  0x12 34 56 78
+     * 
+     *  la primera alternativa guardia 0x56 78
+     *  la segunda 0x12 34
+     * 
+     *  por el momento se decidio la primera, guardaria 0x56 78
+     */
+
+    // call memoryUpdateHandler( EVENT::SET, ...REQUIRED PARAMS)
+
+    for  (int i = bytes-1 ; i >= 0 ; i--) {
+        /**
+         * CUIDADO!!! AL NO UTILIZAR transformAddress, TE ESTAS SALTANDO EL CONTROL DE SEGMENTOS
+         */
+        virtualM->mem[ fisicMemoryAccess + i ] = value & 0xFF ;
+        value = value >> 8;        
+    }
 
 }
 
+void setDataToEmpty (VirtualMachine *virtualM, int operand, int value, int bytes) {
+    error_handler.invalidInstruction();
+}
+
 void initializeSetters () {
+    availableDataSetter[0] = setDataToEmpty;
+    availableDataSetter[1] = setDataToRegister;
+    availableDataSetter[2] = setDataToInmediato;
     availableDataSetter[3] = setDataToMemory;
 }
