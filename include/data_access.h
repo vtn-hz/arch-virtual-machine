b0d25c71933fa219f6e87f6ff6a7b31134e1f833
vtn-hz <juaniyvalen1234@gmail.com>
2025-09-10 23:26:08 -0300

    fix: missed argument on function def

--- include/data_access.h
+++ include/data_access.h
@@ -4,7 +4,7 @@
 typedef struct VirtualMachine VirtualMachine;
 
 int  getData(VirtualMachine*, int operand, int bytes);
-void setData(VirtualMachine*, int operand, int bytes);
+void setData(VirtualMachine*, int operand, int value, int bytes);
 
 typedef int(*p_getter_data)(VirtualMachine*, int operand, int bytes);
 typedef void(*p_setter_data)(VirtualMachine*, int operand, int value, int bytes);
