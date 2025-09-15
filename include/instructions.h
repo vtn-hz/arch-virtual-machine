74b44c86418b9672668f878a1c47c3e16c992fe1
marina <lombardomarina46@gmail.com>
2025-09-14 23:50:25 -0300

    ref: added new instructions functions headers

--- include/instructions.h
+++ include/instructions.h
@@ -5,6 +5,12 @@ typedef struct VirtualMachine VirtualMachine;
 
 typedef void(*p_instruction)(VirtualMachine*);
 
+void MOV (VirtualMachine*);
+void ADD (VirtualMachine*);
+void SUB (VirtualMachine*);
+void MUL (VirtualMachine*);
+void DIV (VirtualMachine*);
+void CMP (VirtualMachine*);
 void SHL (VirtualMachine*);
 void SHR (VirtualMachine*);
 void SAR (VirtualMachine*);  
@@ -15,5 +21,8 @@ void SWAP (VirtualMachine*);
 void LDL (VirtualMachine*);
 void LDH (VirtualMachine*);
 void RND (VirtualMachine*);
+void SYS (VirtualMachine*);
+void NOT (VirtualMachine*);
+void STOP (VirtualMachine*);
 
 #endif
