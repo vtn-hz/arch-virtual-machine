20a70180d4b5ab6bdfc3f0e482693c9857b099bc
sim-schleider <simschleider@gmail.com>
2025-09-14 22:20:12 -0300

    feat: jump instructions

--- include/instructions.h
+++ include/instructions.h
@@ -14,6 +14,12 @@ void XOR (VirtualMachine*);
 void SWAP (VirtualMachine*);
 void LDL (VirtualMachine*);
 void LDH (VirtualMachine*);
-void RND (VirtualMachine*);
+void JMP (VirtualMachine*);
+void JZ (VirtualMachine*);
+void JP (VirtualMachine*);
+void JN (VirtualMachine*);
+void JNZ (VirtualMachine*);
+void JNP (VirtualMachine*);
+void JNN (VirtualMachine*);
 
 #endif
