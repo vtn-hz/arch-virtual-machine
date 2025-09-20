85b2d789d1e6f2ee4e1f59ba06a20e1a47bbd175
vtn-hz <juaniyvalen1234@gmail.com>
2025-09-20 12:36:38 -0300

    ref: ej7.asm

--- test/asm/Ej7.asm
+++ test/asm/Ej7.asm
@@ -1,39 +1,34 @@
-; LEER [0]
-; RESULTADO [4]
-; EFX va a guardar el contador
+        MOV EDX, DS
+        LDH ECX, 4
+        LDL ECX, 1
+        MOV EAX, 1
 
+        SYS 1
 
-            ; Leo el numero que esta en la posicion 0 de memoria
-            MOV EFX, 2
-            MOV EDX, DS
-            LDL ECX, 1
-            LDH ECX, 4
-            MOV EAX, 0x01
-            SYS 1
-            MOV EBX, [EDX]
+        MOV [4], [0]
+        DIV [4], 2
 
-            ; Si es 2 cuenta como primo
-            CMP [EDX], 2
-            JZ FIN
+OTRO:   CMP [4], 1
+        JNP FIN
+        CMP AC, 0
+        JZ FIN
 
-OTRO:       CMP EBX, EFX ; Resto el numero con el contador
-            JNP PRIMO ; Si hice n - n es porque ya no hay numeros para probar
-            DIV EBX, EFX
-            CMP AC, 0 ; Si el resto de hacer la division da 0 (Se guarda en AC) encontre un divisor, termino
-            JZ NO_PRIMO
-            ADD EFX, 1
-            JMP OTRO
+        MOV [8], [0]
+        SUB [4], 1
+        DIV [8], [4]
+        JMP OTRO
 
-PRIMO:      MOV EBX, 1
-            JMP FIN
-    
-NO_PRIMO:   MOV EBX, 0
+FIN:    MOV [12], 0
+        CMP [4], 1
+        JNZ NOP
+        MOV [12], 1
 
-FIN:        MOV EDX, DS
-            ADD EDX, 4
-            MOV [EDX], EBX
-            LDL ECX, 1
-            LDH ECX, 4
-            MOV EAX, 0x01
-            SYS 2
-            STOP
+NOP:    MOV EDX, DS
+        ADD EDX, 12
+        LDH ECX, 4
+        LDL ECX, 1
+        MOV EAX, 1
+
+        SYS 2
+
+        STOP
