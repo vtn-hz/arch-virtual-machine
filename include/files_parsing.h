7c2df14521f0476e5982cc9156ac86e5e0662cb2
marina <lombardomarina46@gmail.com>
2025-10-17 12:01:12 -0300

    ref: readSizes header declaration added

--- include/files_parsing.h
+++ include/files_parsing.h
@@ -5,6 +5,7 @@ void getParsed(char** codeSegmentContent, char** constSegmentContent,  arguments
 void vmxVersionOne(FILE* file, char **codeSegmentContent, int sizes[]);
 void vmxVersionTwo(FILE* file,char **codeSegmentContent, char** constSegmentContent, int* entryPoint, int sizes[]);
 void vmiVersionOne(FILE* file, char** codeSegmentContent, arguments* args, int sizes[]);
+void readSizes(FILE* file, int sizes[]);
 void readIdentifier(FILE* file, int* version);
 
 #endif
