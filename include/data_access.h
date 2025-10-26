0aa70946a7cfe69d8e770e94179202d0fc9d583d
vtn-hz <juaniyvalen1234@gmail.com>
2025-10-26 18:03:59 -0300

    ref: implemented executeDataPop & executeDataPush method to keep DRY principe

--- include/data_access.h
+++ include/data_access.h
@@ -9,9 +9,13 @@ int  getData(VirtualMachine*, int operand);
 void prepareGetMemoryAccess(VirtualMachine*, int baseRegister, int memoryOffset, int bytes);
 int  commitGetMemoryAccess(VirtualMachine*);
 
+int executeDataPop(VirtualMachine*);
+
 void setData(VirtualMachine*, int operand, int value);
 void prepareSetMemoryAccess(VirtualMachine*, int baseRegister, int memoryOffset, int value, int bytes);
 void commitSetMemoryAccess(VirtualMachine*);
+
+void executeDataPush(VirtualMachine*, int);
 
 typedef int (*p_getter_data)(VirtualMachine*, int operand);
 typedef void (*p_setter_data)(VirtualMachine*, int operand, int value);
